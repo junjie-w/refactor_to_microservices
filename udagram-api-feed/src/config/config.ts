841af9b3a78016144488dfdb970009978ddc5978
Wu <wujunjie@Wus-MacBook-Pro.local>
2022-02-17 08:59:00 +0100

    updated config files

--- udagram-api-feed/src/config/config.ts
+++ udagram-api-feed/src/config/config.ts
@@ -1,14 +1,14 @@
 export const config = {
-  'username': 'chirperadmin',
-  'password': 'postgres',
-  'database': 'chirperdb',
-  'host': 'wyr.cfjeibujcbd0.us-east-1.rds.amazonaws.com',
+  'username': 'junjie',
+  'password': 'Bauhaus_01',
+  'database': 'postgres',
+  'host': 'jw.chmsx11y5ipe.us-east-1.rds.amazonaws.com',
   'dialect': 'postgres',
   'aws_region': 'us-east-1',
   'aws_profile': 'default',
-  'aws_media_bucket': 'test-376262390406-dev',
+  'aws_media_bucket': 'jw-139779971562-dev',
   'url': 'http://localhost:8100',
   'jwt': {
-    'secret': 'hello',
+    'secret': 'testing',
   },
 };
